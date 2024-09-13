5b6bbc91f25f801b7ff7ad4e1fbf6c39b46a81bf
Nirav Shah <nirav@niravs-mbp.lan>
2024-09-13 03:54:45 -0700

    Fixed typos. Added stack name to server output

--- start-service.sh
+++ start-service.sh
@@ -6,10 +6,10 @@ cd /home/ec2-user/app/release
 REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
 
 #Query the EC2 metadata service for this instance's instance-id
-export INSTANCE_ID =`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 
 #Query EC2 describe tags method and pull stack-name
-export STACK_NAME = `aws --region $REGION ec2 describe-tags \
+export STACK_NAME=`aws --region $REGION ec2 describe-tags \
     --filters "Name=resource-id,Values=${INSTANCE_ID}" \
               "Name=key,Values=aws:cloudformation:stack-name" \
     | jq -r ".Tags[0].Value"`
