ae743b446104ef8d96b2c0df415768708aee5be2
Nirav Shah <nirav@niravs-mbp.lan>
2024-09-13 08:40:57 -0700

    Split out staging nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -10,6 +10,7 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
     --query "Account" --output text`
 
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
 
 GH_ACCESS_TOKEN=$(cat ~/dev/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/dev/.github/aws-bootstrap-owner)
@@ -27,7 +28,25 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+#package up Cloudformation templates into an S3 bucket
+echo -e "\n\n=========== Packaging main.yml ===========\n"
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --template main.yml \
+    --s3-bucket $CFN_BUCKET \
+    --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+    echo "ERROR while running 'aws cloudformation package' command: "
+    echo $PACKAGE_ERR
+    exit 1
+fi
 
 
 #Deploy the CloudFormation template
@@ -40,7 +59,7 @@ aws cloudformation deploy \
     --region $REGION \
     --profile $CLI_PROFILE \
     --stack-name $STACK_NAME \
-    --template-file main.yml \
+    --template-file ./cfn_output/main.yml \
     --disable-rollback \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
@@ -53,14 +72,10 @@ aws cloudformation deploy \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 
-# If the deploy succeeded, show the DNS name of the created instance
+# If the deploy succeeded, show the DNS name of the endpoints
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
-
-  aws cloudformation list-exports \
-    --profile awsbootstrap \
-    --query "Exports[?starts_with(Name,'LBEndpoint')].Value"
+    --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
   # starts_with is not necessary right now, but will be useful when we get to the Production section
 fi
